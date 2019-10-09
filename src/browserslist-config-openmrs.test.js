d437c2a677adef953ae25f2bc07e06b2c7b6fc63
Joel Denning <fake@fake.com>
2019-10-09 17:41:19 -0600

    Upgrading dependencies and fixing typo

--- src/browserslist-config-openmrs.test.js
+++ src/browserslist-config-openmrs.test.js
@@ -1,7 +1,7 @@
 const browserslist = require("browserslist");
 const queries = require("./browserslist-config-openmrs.js");
 
-describe("browserslist-config-canopy", () => {
+describe("browserslist-config-openmrs", () => {
   it(`exports a valid browserslist config that is what we want for OpenMRS`, () => {
     const browsers = browserslist(queries);
     expect(browsers).toMatchSnapshot();
